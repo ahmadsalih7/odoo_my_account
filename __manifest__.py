93e46a78b181e02600f6a604ba6a731734dc40b2
Ahmad Salih <ahmadsalih7@hotmail.com>
2021-11-15 16:54:34 +0300

    Define Account Tags

--- __manifest__.py
+++ __manifest__.py
@@ -27,7 +27,8 @@
         'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
-        'views/myaccount_menus.xml'
+        'views/myaccount_menus.xml',
+        'data/account_tags.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
--- models/my_account_models.py
+++ models/my_account_models.py
@@ -9,8 +9,17 @@ class my_account(models.Model):
 
     name = fields.Char(string='Name')
     code = fields.Char(string='Code')
+    tag_ids = fields.Many2many('myaccount.myaccount.tag', 'name', string='Tags')
     reconcile = fields.Boolean(string='Allow Reconciliation')
     deprecate = fields.Boolean(string='Deprecated')
+
+
+class my_account(models.Model):
+    _name = 'myaccount.myaccount.tag'
+    _description = 'tags of accounts '
+
+    name = fields.Char(string='Name', required=True)
+    
 
 #     @api.depends('value')
 #     def _value_pc(self):
