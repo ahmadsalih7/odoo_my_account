e2a6ac51cf7dbd9af48207e95dd12b145325ca3f
Ahmad Salih <ahmadsalih7@hotmail.com>
2021-11-18 19:08:11 +0300

    Create Journal Entries model
    and related menues and views.

--- models/journal_entry.py
+++ models/journal_entry.py
@@ -14,3 +14,13 @@ class my_accountMoveLine(models.Model):
     company_currency_id = fields.Many2one(related='company_id.currency_id', string='Company Currency', readonly=True, store=True)    
     debit = fields.Monetary(string='Debit', default=0.0, currency_field='company_currency_id')
     credit = fields.Monetary(string='Credit', default=0.0, currency_field='company_currency_id')
+    move_id = fields.Many2one('myaccount.move')
+
+class my_accountMove(models.Model):
+    _name = "myaccount.move"
+    _description = "Journal Entries"
+
+    date = fields.Date(string='Date', required=True, index=True, readonly=True, default=fields.Date.context_today)
+    ref = fields.Char(string='Reference', copy=False)
+    line_ids = fields.One2many('myaccount.move.line', 'move_id',string='Journal Items')
+
