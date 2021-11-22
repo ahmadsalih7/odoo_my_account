a70a6600ade00b40a516046ea0f5d8d48196dbe6
Ahmad Salih <ahmadsalih7@hotmail.com>
2021-11-23 00:09:16 +0300

    Add balance field to  auto populate credit/debit
    So Entries in Journal Entry be balanced

--- models/journal_entry.py
+++ models/journal_entry.py
@@ -15,6 +15,34 @@ class my_accountMoveLine(models.Model):
     debit = fields.Monetary(string='Debit', default=0.0, currency_field='company_currency_id')
     credit = fields.Monetary(string='Credit', default=0.0, currency_field='company_currency_id')
     move_id = fields.Many2one('myaccount.move')
+    balance = fields.Monetary(string='Balance', default=0.0, currency_field='company_currency_id')
+
+    ####################
+    # Helper Functions
+    ####################
+    @api.model
+    def get_balance(self):
+        return self.move_id.line_ids[0].balance
+
+    ####################
+    # On change methods
+    ####################
+
+    @api.onchange('debit')
+    def onChangeDebit(self):
+        old_balance = self.get_balance()
+        if old_balance == 0.0 and self.debit != 0.0:
+            self.balance = -1 * self.debit
+        elif old_balance > 0.0 and self.debit == 0.0:
+            self.debit = old_balance
+
+    @api.onchange('credit')
+    def onChangeCredit(self):
+        old_balance = self.get_balance()
+        if old_balance == 0.0 and self.credit != 0.0:
+            self.balance = self.credit
+        elif old_balance < 0.0 and self.credit == 0.0:
+            self.credit = -1 * old_balance
 
 class my_accountMove(models.Model):
     _name = "myaccount.move"
